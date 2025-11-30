c63731eb9cfbe94a3ef6bc1b34d4b47fb387813f
Leo <nn6121040@gs.ncku.edu.tw>
2025-12-01 00:59:49 +0800

    fix: update tailwind purge path

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    './*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './App.tsx',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
